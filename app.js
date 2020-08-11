f3b4c3d7cd38cde6231847144b9eae4c4e7447e4
shahzaibhabib7 <mail.shahzaibhabib7@gmail.com>
2020-08-11 12:06:49 +0500

    Added our own middleware along with a logging middleware using morgan package

--- app.js
+++ app.js
@@ -2,13 +2,28 @@
 const fs = require('fs');
 // third party modules
 const express = require('express');
+const morgan = require('morgan');
 // own modules
 
 
 const app = express();
 
+// 1) BODY PARSER MIDDLEWARE
 // data from the body is added to the 'req' object
 app.use(express.json());
+
+// our own middleware 
+// app.use((req, res, next) => {
+//     console.log('Hello, from the middleware');
+//     next();
+// });
+
+// app.use((req, res, next) => {
+//     req.requestTime = new Date().toISOString();
+//     next();
+// });
+
+app.use(morgan('dev'));
 
 const projects = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/project-simple.json`));
 
